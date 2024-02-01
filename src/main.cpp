4b582f70f69627e83c2953809c0858fb63038658
Jason Klapste <jklapste@gmail.com>
2024-02-01 14:03:49 -0600

    addl compile-time configuration

--- src/main.cpp
+++ src/main.cpp
@@ -916,7 +916,9 @@ void setup()
   {
     ESP_LOGD(TAG, "Issuer ID: %s, Public Key: %s", utils::bufToHexString(issuer.issuerId, sizeof(issuer.issuerId)).c_str(), utils::bufToHexString(issuer.publicKey, sizeof(issuer.publicKey)).c_str());
   }
-  homeSpan.setHostNameSuffix("-homekey");
+#ifdef HOSTNAME_SUFFIX
+  homeSpan.setHostNameSuffix(HOSTNAME_SUFFIX);
+#endif
 #ifdef OTA_AUTH
   homeSpan.enableOTA(OTA_AUTH);
 #endif
